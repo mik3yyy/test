bdfebdc47351102d35fe7a696a8b55c1ee1d4c7c
youpelegrace <youpeleg@gmail.com>
2022-04-12 08:05:16 +0100

    auth fix

--- lib/presentation/screens/auth/sign_in/sign_in.dart
+++ lib/presentation/screens/auth/sign_in/sign_in.dart
@@ -8,7 +8,6 @@ import 'package:kayndrexsphere_mobile/presentation/components/reusable_widget.da
 import 'package:kayndrexsphere_mobile/presentation/route/navigator.dart';
 import 'package:kayndrexsphere_mobile/presentation/screens/auth/create_acount/create_account.dart';
 import 'package:kayndrexsphere_mobile/presentation/components/text%20field/text_form_field.dart';
-import 'package:kayndrexsphere_mobile/presentation/screens/auth/transaction_pin/transaction_pin.dart';
 import 'package:kayndrexsphere_mobile/presentation/screens/auth/vm/sign_in_vm.dart';
 import 'package:kayndrexsphere_mobile/presentation/screens/home/home.dart';
 import 'package:kayndrexsphere_mobile/presentation/screens/home/widgets/main_screen.dart';
@@ -17,15 +16,12 @@ import 'package:loader_overlay/loader_overlay.dart';
 
 import '../../../../Data/controller/controller/generic_state_notifier.dart';
 import '../../../components/AppSnackBar/snackbar/app_snackbar_view.dart';
+import '../transaction_pin/transaction_pin.dart';
 
 class SigninScreen extends HookConsumerWidget {
   SigninScreen({Key? key}) : super(key: key);
 
-
   final formKey = GlobalKey<FormState>();
-
-               
-
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -34,7 +30,7 @@ class SigninScreen extends HookConsumerWidget {
     final passwordController = TextEditingController();
     ref.listen<RequestState>(signInProvider, (T, value) {
       if (value is Success) {
-        context.navigate(HomePage());
+        context.navigate(TransactionPinScreen());
         return AppSnackBar.showSuccessSnackBar(
           context,
           message: "Login successful!",
@@ -61,135 +57,109 @@ class SigninScreen extends HookConsumerWidget {
               padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 50.h),
               child: Form(
                 key: formKey,
-                child: Column(
-                  children: [
-                    Text(
-                      "Welcome Back!",
-                      style:
-                          AppText.header3(context, AppColors.appColor, 20.sp),
-                      textAlign: TextAlign.center,
-                    ),
-                    Space(12.h),
-                    Text(
-                      "Continue to Sign in",
-                      style:
-                          AppText.header2(context, AppColors.appColor, 20.sp),
-                      textAlign: TextAlign.center,
-                    ),
-                    Space(180.h),
+                child: Column(children: [
+                  Text(
+                    "Welcome Back!",
+                    style: AppText.header3(context, AppColors.appColor, 20.sp),
+                    textAlign: TextAlign.center,
+                  ),
+                  Space(12.h),
+                  Text(
+                    "Continue to Sign in",
+                    style: AppText.header2(context, AppColors.appColor, 20.sp),
+                    textAlign: TextAlign.center,
+                  ),
+                  Space(180.h),
 
-
-                    //email
-                    TextFormInput(
-                        textAlign: TextAlign.center,
-                        labelText: 'Email or Phone Number',
-                        controller: emailPhoneController,
-                        validator: (value) {},
-                        obscureText: false),
-                    Space(32.h),
-
-                    //password
-                    TextFormInput(
+                  //email
+                  TextFormInput(
                       textAlign: TextAlign.center,
-                      labelText: 'Password',
-                      controller: passwordController,
+                      labelText: 'Email or Phone Number',
+                      controller: emailPhoneController,
                       validator: (value) {},
-                      obscureText: false,
-                      suffixIcon: Icon(Icons.visibility),
+                      obscureText: false),
+                  Space(32.h),
 
-                Space(160.h),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomButton(
-                      buttonWidth: 280.w,
-                      buttonText: 'Sign In',
-                      bgColor: AppColors.appColor,
-                      borderColor: AppColors.appColor,
-                      textColor: Colors.white,
-                      onPressed: () {
-                        context.navigate(
-                          MainScreen(
-                            menuScreenContext: context,
-                          ),
-                        );
-                      },
-
-                    ),
-                    Space(32.h),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Forgot Password?",
-                          style: AppText.body4(context, AppColors.appColor),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-
-                    Space(160.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CustomButton(
-                          buttonWidth: 280.w,
-                          buttonText: 'Sign In',
-                          bgColor: AppColors.appColor,
-                          borderColor: AppColors.appColor,
-                          textColor: Colors.white,
-                          onPressed: vm is Loading
-                              ? null
-                              : () {
-                                  if (formKey.currentState!.validate()) {
-                                    ref.read(signInProvider.notifier).signIn(
-                                        emailPhoneController.text,
-                                        passwordController.text);
-                                    context.loaderOverlay.show();
-                                  }
-                                },
-                        ),
-                        Container(
-                          height: 55.h,
-                          width: 80.w,
-                          decoration: BoxDecoration(
-                            color: AppColors.appColor,
-                            borderRadius: BorderRadius.circular(6.r),
-                          ),
-                          child: const Image(
-                            image: AssetImage(
-                              "images/fingerprint-3.png",
-                            ),
-                            color: AppColors.whiteColor,
-                          ),
-                        )
-                      ],
-                    ),
-                    Space(20.h),
-                    Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(
-                              text: 'Don’t have an account? ',
-                              style:
-                                  AppText.body4(context, AppColors.appColor)),
-                          WidgetSpan(
-                            child: InkWell(
-                              onTap: () =>
-                                  context.navigate(CreateAccountScreen()),
-                              child: Text(
-                                ' Sign up',
-                                style:
-                                    AppText.body4(context, AppColors.appColor),
-                              ),
-                            ),
-                          ),
-                        ],
+                  //password
+                  TextFormInput(
+                    textAlign: TextAlign.center,
+                    labelText: 'Password',
+                    controller: passwordController,
+                    validator: (value) {},
+                    obscureText: false,
+                    suffixIcon: Icon(Icons.visibility),
+                  ),
+                  Space(32.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password?",
+                        style: AppText.body4(context, AppColors.appColor),
+                        textAlign: TextAlign.center,
                       ),
-                    )
-                  ],
-                ),
+                    ],
+                  ),
+
+                  Space(160.h),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(
+                        buttonWidth: 280.w,
+                        buttonText: 'Sign In',
+                        bgColor: AppColors.appColor,
+                        borderColor: AppColors.appColor,
+                        textColor: Colors.white,
+                        onPressed: vm is Loading
+                            ? null
+                            : () {
+                                if (formKey.currentState!.validate()) {
+                                  ref.read(signInProvider.notifier).signIn(
+                                      emailPhoneController.text,
+                                      passwordController.text);
+                                  context.loaderOverlay.show();
+                                }
+                              },
+                      ),
+                      Container(
+                        height: 55.h,
+                        width: 80.w,
+                        decoration: BoxDecoration(
+                          color: AppColors.appColor,
+                          borderRadius: BorderRadius.circular(6.r),
+                        ),
+                        child: const Image(
+                          image: AssetImage(
+                            "images/fingerprint-3.png",
+                          ),
+                          color: AppColors.whiteColor,
+                        ),
+                      )
+                    ],
+                  ),
+                  Space(20.h),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'Don’t have an account? ',
+                            style: AppText.body4(context, AppColors.appColor)),
+                        WidgetSpan(
+                          child: InkWell(
+                            onTap: () =>
+                                context.navigate(CreateAccountScreen()),
+                            child: Text(
+                              ' Sign up',
+                              style: AppText.body4(context, AppColors.appColor),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
               ),
             ),
           ),
