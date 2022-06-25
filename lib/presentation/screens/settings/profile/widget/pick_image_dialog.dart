14f9075407fd8aba81a54f3a9db3c0cb7410954b
deecodez <obarodayo@gmail.com>
2022-06-25 22:18:29 +0100

    fixed overflow on image picker dialog

--- lib/presentation/screens/settings/profile/widget/pick_image_dialog.dart
+++ lib/presentation/screens/settings/profile/widget/pick_image_dialog.dart
@@ -20,7 +20,7 @@ class PickImageDialog {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(5.r))),
                 content: SizedBox(
-                    height: 130.h,
+                    height: 180.h,
                     width: 20.w,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
